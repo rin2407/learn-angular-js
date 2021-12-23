0df92b683c919a030683eae25a9d1212be0913e1
rin2407 <vanrin24071998@gmail.com>
2021-12-23 14:24:43 +0700

    form group

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  public name = new FormControl('');
+  // form group
+  public profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+  // public updateName() {
+  //   this.name.setValue('Nancy');
+  // }
+  public onSubmit() {
+    console.log(this.profileForm.value);
   }
-
+  ngOnInit(): void {}
 }
